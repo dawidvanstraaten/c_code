4c8017f40a93839c656c245e5e5fd19956acc1f7
dawidvanstraaten <dawidvanstraaten@gmail.com>
2021-03-11 13:11:39 +0200

    A first program in C.

---
+++ first.c
@@ -0,0 +1,13 @@
+/*
+    A first program in C. This a
+    a multi-line comment.
+*/
+#include <stdio.h>
+
+// Program execution always starts at function main.
+int main(void) {
+
+    printf("Hello, C!\n");
+
+    return 0; // Function completed successfully.
+}
